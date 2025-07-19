6a8d6212fb910a0a78878275ea4aef5f7f10eaed
Manish <mk5758029@gmail.com>
2025-07-19 22:20:21 +0530

    add product

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,87 @@
-import { foodCategories } from '@/components/carte'
+import { foodCategories, popularItems } from '@/components/carte'
+import foodList from '@/components/foodList.json'
 import Navbar from '@/components/navbar'
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { moderateScale } from 'react-native-size-matters'
-
+import { moderateScale, scale, verticalScale } from 'react-native-size-matters'
+const { width } = Dimensions.get('window')
 const index = () => {
+  console.log(foodList)
+
   return (
     <SafeAreaView>
       <View>
         <Navbar />
       </View>
-      {/* start main container */}
-      <View style={{ paddingHorizontal: moderateScale(10) }}>
 
-        <View>
-          <Text style={{ fontWeight: 600, fontSize: moderateScale(22), marginVertical: moderateScale(7)}}>
-            Category
-          </Text>
-          <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: moderateScale(10) }}>
-            {foodCategories.map((data, index) => {
-              return (
-                <View key={index} style={{ width: '48%', height:'auto',backgroundColor:'white' ,borderRadius:moderateScale(10)}}>
-                  <View style={{ width: '100%', height:moderateScale(150)}}>
-                    <Image source={{ uri: data.image }} style={{ width: '100%', height: '100%', resizeMode: 'cover' ,borderTopRightRadius:10 ,borderTopLeftRadius:10 }} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* start main container */}
+        <View style={{ paddingHorizontal: moderateScale(10) }}>
+
+          <View>
+            <Text style={{ fontWeight: 600, fontSize: moderateScale(22), marginVertical: moderateScale(7) }}>
+              Categories
+            </Text>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: moderateScale(10) }}>
+              {foodCategories.map((data, index) => {
+                return (
+                  <View key={index} style={{ width: '48%', height: 'auto', backgroundColor: 'white', borderRadius: moderateScale(10) }}>
+                    <View style={{ width: '100%', height: moderateScale(150) }}>
+                      <Image source={{ uri: data.image }} style={{ width: '100%', height: '100%', resizeMode: 'cover', borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                    </View>
+                    <View style={{ paddingHorizontal: moderateScale(15), paddingVertical: moderateScale(7) }}>
+                      <Text style={{ fontSize: moderateScale(17), fontWeight: 500 }}>{data.category}</Text>
+                    </View>
                   </View>
-                  <View style={{paddingHorizontal:moderateScale(15) ,paddingVertical:moderateScale(7) }}>
-                    <Text style={{fontSize:moderateScale(17),fontWeight:500}}>{data.category}</Text>
-                  </View>
-                </View>
-              )
-            })}
+                )
+              })}
+            </View>
           </View>
-        </View>
+          {/* popular item list start*/}
+          <View>
+            <Text style={{ fontWeight: 600, fontSize: moderateScale(22), marginVertical: moderateScale(7) }}>
+              Popular Items
+            </Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {popularItems.flatMap((cate) =>
+                cate.topItems.map((data: any, index) => (
+                  <View style={{ width: scale(80), height: verticalScale(70), marginLeft: moderateScale(data.ml) }} key={`${cate.id}-${index}`}>
+                    <Image source={{ uri: data.image }} style={{ width: '100%', height: '100%', resizeMode: 'cover', borderRadius: moderateScale(50) }} />
+                  </View>
+                ))
+              )}
+            </ScrollView>
+          </View>
+          {/* popular item list end*/}
 
-        <Text>try</Text>
+          {/* print whole json data start */}
+
+          <View>
+            <Text style={{ fontWeight: 600, fontSize: moderateScale(22), marginVertical: moderateScale(7) }}>
+              Categories
+            </Text>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: moderateScale(10) }}>
+              {foodList.map((data, index) => {
+                return (
+                  <View key={index} style={{ width: '48%', height: 'auto', backgroundColor: 'white', borderRadius: moderateScale(10) }}>
+                    <View style={{ width: '100%', height: moderateScale(150) }}>
+                      <Image source={{ uri: data.image }} style={{ width: '100%', height: '100%', resizeMode: 'cover', borderTopRightRadius: 10, borderTopLeftRadius: 10 }} />
+                    </View>
+                    <View style={{ paddingHorizontal: moderateScale(15), paddingVertical: moderateScale(7) }}>
+                      <Text style={{ fontSize: moderateScale(17), fontWeight: 500 }}>{data.category}</Text>
+                    </View>
+                  </View>
+                )
+              })}
+            </View>
+          </View>
+          {/* print whole json data start */}
+
+
+        </View>
         {/* end main container */}
-      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- components/carte.tsx
+++ components/carte.tsx
@@ -16,3 +16,95 @@ export const foodCategories =[
       "image": "https://images.unsplash.com/photo-1559847844-5315695dadae?w=500&auto=format&fit=crop",
     }
   ]
+
+
+
+export const popularItems = [
+  {
+    "category": "Italian",
+    "id": 1,
+    "image": "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=500&auto=format&fit=crop",
+    "topItems": [
+      {
+        "name": "Margherita Pizza",
+        "image": "https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?w=500&auto=format&fit=crop"
+      },
+      {
+        "name": "Spaghetti Carbonara",
+        "image": "https://images.unsplash.com/photo-1555949258-eb67b1ef0ceb?w=500&auto=format&fit=crop",
+        'ml':10
+      },
+      {
+        "name": "Tiramisu",
+        "image": "https://images.unsplash.com/photo-1563805042-7684c019e1cb?w=500&auto=format&fit=crop",
+        'ml':10
+      }
+    ]
+  },
+  {
+    "category": "Indian",
+    "id": 2,
+    "image": "https://images.unsplash.com/photo-1585937421612-70a008356fbe?w=500&auto=format&fit=crop",
+    "topItems": [
+      {
+        "name": "Butter Chicken",
+        "image": "https://img.taste.com.au/qPAElV3h/taste/2013/07/easy-butter-chicken-image1-197844-1.jpg",
+        'ml':10,
+      },
+      {
+        "name": "Biryani",
+        "image": "https://images.unsplash.com/photo-1631515243349-e0cb75fb8d3a?w=500&auto=format&fit=crop",
+        'ml':10,
+      },
+      {
+        "name": "Masala Dosa",
+        "image": "https://culinarydelightsandbeyond.com/wp-content/uploads/2023/03/dosa-5oF7d_hPJG4-scaled.jpg",
+        'ml':10
+      }
+    ]
+  },
+  {
+    "category": "Chinese",
+    "id": 3,
+    "image": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop",
+    "topItems": [
+      {
+        "name": "Dim Sum",
+        "image": "https://images.unsplash.com/photo-1607532941433-304659e8198a?w=500&auto=format&fit=crop",
+        'ml':10
+      },
+      {
+        "name": "Manchurian",
+        "image": "https://atanurrannagharrecipe.com/wp-content/uploads/2023/01/Veg-Manchurian-for-website.jpg",
+        'ml':10
+      },
+      {
+        "name": "Hakka Noodles",
+        "image": "https://images.unsplash.com/photo-1585032226651-759b368d7246?w=500&auto=format&fit=crop",
+        'ml':10
+      }
+    ]
+  },
+  {
+    "category": "Street Food",
+    "id": 4,
+    "image": "https://images.unsplash.com/photo-1559847844-5315695dadae?w=500&auto=format&fit=crop",
+    "topItems": [
+      {
+        "name": "Pani Puri",
+        "image": "https://images.unsplash.com/photo-1603105037880-880cd4edfb0d?w=500&auto=format&fit=crop",
+        'ml':10
+      },
+      {
+        "name": "Vada Pav",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDmGkPbMl9EwqNvX6-kmJffgtRv2KMEEd9_A&s",
+        'ml':10
+      },
+      {
+        "name": "Chole Bhature",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRTltZRK6Nb2hlLcg-PySvkjbxAJdEx_0zcQ&s",
+        'ml':10
+      }
+    ]
+  }
+];
